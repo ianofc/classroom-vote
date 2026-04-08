c88e873ca5409e4a14a0c875a90038a288bc9276
Ian Santos <issmovel@gmail.com>
2026-04-08 10:19:03 -0300

    Add new routes for Cadastro and Telao pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Cadastro from "./pages/Cadastro"; // <-- IMPORTAÇÃO DA NOVA PÁGINA
+import Cadastro from "./pages/Cadastro";
+import Telao from "./pages/Telao"; // <-- IMPORTAÇÃO DO NOVO MODO TELÃO
 
 const queryClient = new QueryClient();
 
@@ -20,6 +21,9 @@ const App = () => (
           
           {/* NOVA ROTA DE CADASTRO PARA O SAAS */}
           <Route path="/cadastro" element={<Cadastro />} />
+          
+          {/* NOVA ROTA DO MODO TELÃO (TEMPO REAL) */}
+          <Route path="/telao" element={<Telao />} />
           
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
